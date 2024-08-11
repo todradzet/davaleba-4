878e263f0408adf0c38050c97a86294a29cd8fb6
todradzet <tamta.todradze.1@iliauni.edu.ge>
2024-08-11 13:44:15 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let age = 20;
+
+if (isNaN(age)) {
+    console.log("Indicated age is incorrect!");
+} else if (age < 18) {
+    console.log("You can't get a driver's license");
+} else {
+    console.log("You can get a driver's license");
+}
